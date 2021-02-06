b5bb42ddc96bfab34d0d4d20c703f8f6ba89e2d0
student <smodarressi7@gmail.com>
2021-02-06 18:22:09 +0000

    Spring something

---
+++ RevewAnalyser/src/main/java/com/example/RevewAnalyser/RevewAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.example.RevewAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevewAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevewAnalyserApplication.class, args);
+	}
+
+}
---
+++ RevewAnalyser/src/test/java/com/example/RevewAnalyser/RevewAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RevewAnalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevewAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
