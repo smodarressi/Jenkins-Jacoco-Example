3e4766f0687a4ac63a1caf29d0186977c3bfbba2
student <smodarressi7@gmail.com>
2021-02-06 18:41:03 +0000

    add logic and test jacoco

--- RevewAnalyser/src/test/java/com/example/RevewAnalyser/RevewAnalyserApplicationTests.java
+++ RevewAnalyser/src/test/java/com/example/RevewAnalyser/RevewAnalyserApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.RevewAnalyser;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RevewAnalyserApplicationTests {
+class ReviewAnalyserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
 }
